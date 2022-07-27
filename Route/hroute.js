3a906bc49135c0de5d2767b833ae1b492d2bd346
Wazzaubi <joeyliu7886@gmail.com>
2022-07-26 21:49:21 -0700

    nodemon success

--- Route/hroute.js
+++ Route/hroute.js
@@ -2,11 +2,11 @@ const route = require('express').Router();
 const path = require("path");
 
 route.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname,"/Develop/public/index.html"))
+    res.sendFile(path.join(__dirname,"../Develop/public/index.html"))
 })
 
 route.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname,"/Develop/public/notes.html")) 
+    res.sendFile(path.join(__dirname,"../Develop/public/notes.html")) 
 })
 
 module.exports = route
