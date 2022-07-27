a358bd1237b662e3a9a6d379df5a654fa0679524
Wazzaubi <joeyliu7886@gmail.com>
2022-07-26 22:16:50 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# M10-Note-Taker
+# M11 Note Taker
+
+## My task and what I learned
+- Utilize my skills with node.js and express.js to a create a live server.
+- Had a lot of trouble with the npm installs and how to set certain const
+- Unsure why my text boxes are formatted weirdly.
+- unable to get save button to work
+
+## URL
+- Deployed URL:https://joeyliu518.github.io/M10-Note-Taker/
+- Github URL: https://github.com/JoeyLiu518/JS-password-generator
+
+## Screenshot
+- <img src="./Assets/images/Screenshot.png">
--- Route/aroute.js
+++ Route/aroute.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid')
 
 // Created a route that gets the data of user which will then parse into a JSON object
 route.get("/api/notes", (req, res) => {
-    const data = fs.readFileSync(".develop/db/db.json");
+    const data = fs.readFileSync("./Develop/db/db.json");
     res.json(JSON.parse(data));
 });
 
@@ -14,19 +14,19 @@ route.post("/api/notes", (req, res) => {
     // This is the package request
     const writeNote = req.body;
     // Parses JSON and allows us to grab object
-    const note = JSON.parse(fs.readFileSync(".develop/db/db.json"));
+    const note = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
 
     writeNote.id = uuid;
     note.push(writeNote);
     //Writes stringifed note into our db folder.json
-    fs.writeFileSync(".develop/db/db.json", JSON.stringify(note));
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(note));
     res.json(note)
     //Updates notes
 });
 
 // Need a way to delete note. 
 route.delete("/api/notes:id", (req,res) => {
-    let note = JSON.parse(fs.readFileSync(".develop/db/db.json"));
+    let note = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
     const deleteNote = (req.params.id).toString();
 
     //
@@ -34,7 +34,7 @@ route.delete("/api/notes:id", (req,res) => {
         return removeNote.id != deleteNote
     })
     //
-    fs.writeFileSync(".develop/db/db.json", JSON.stringify(note));
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(note));
     res.json(note);
 })
 
